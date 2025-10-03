6be50679e4a1ff908e77e62fcf4fd4e1e9b41504
eminet666 <eminet666@gmail.com>
2025-10-03 09:30:24 +0200

    x

--- 1_tuto_neural_network/10.6-7_matrix_math_ES5/sketch.js
+++ 1_tuto_neural_network/10.6-7_matrix_math_ES5/sketch.js
@@ -6,34 +6,40 @@ function setup() {
 }
 
 function draw() {
-    
-}
+    var m = new NeuralNetwork(3,2);
+    m.add(5);
+    m.multiply(3);
+    console.table(m.matrix);
 
-// test program
-// var m = new NeuralNetwork(3,2);
-// m.add(5);
-// m.multiply(3);
-// console.table(m.matrix);
-//
-// var m = new NeuralNetwork(3,2);
-// m.randomize();
-// m.multiply(2);
-// console.table(m.matrix);
-//
-// var m1 = new NeuralNetwork(3,2);
-// var m2 = new NeuralNetwork(3,2);
-// m1.randomize();
-// m2.randomize();
-// console.table(m1.matrix);
-// console.table(m2.matrix);
-// m1.add(m2);
-// console.table(m1.matrix);
-//
-// var m1 = new NeuralNetwork(3,2);
-// var m2 = new NeuralNetwork(3,2);
-// m1.randomize();
-// m2.randomize();
-// console.table(m1.matrix);
-// console.table(m2.matrix);
-// m1.multiply(m2);
-// console.table(m1.matrix);
+    // var m = new NeuralNetwork(3,2);
+    // m.randomize();
+    // m.multiply(2);
+    // console.table(m.matrix);
+
+    // var m1 = new NeuralNetwork(3,2);
+    // var m2 = new NeuralNetwork(3,2);
+    // m1.randomize();
+    // m2.randomize();
+    // console.table(m1.matrix);
+    // console.table(m2.matrix);
+    // m1.add(m2);
+    // console.table(m1.matrix);
+
+    // var m1 = new NeuralNetwork(3,2);
+    // var m2 = new NeuralNetwork(3,2);
+    // m1.randomize();
+    // m2.randomize();
+    // console.table(m1.matrix);
+    // console.table(m2.matrix);
+    // m1.multiply(m2);
+    // console.table(m1.matrix);
+
+    // var m1 = new NeuralNetwork(3,2);
+    // var m2 = new NeuralNetwork(3,2);
+    // m1.randomize();
+    // m2.randomize();
+    // console.table(m1.matrix);
+    // console.table(m2.matrix);
+    // m1.multiply(m2);
+    // console.table(m1.matrix); 
+}
